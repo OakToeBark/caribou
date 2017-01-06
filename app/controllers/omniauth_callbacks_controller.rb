2e7da385fb5d0806d5d85f3b393827643582a411
Brock Luker <brockluker@gmail.com>
2017-01-06 09:44:17 -0800

    Small change in the OmniauthCallback - Facebook Authentication still does not work

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -19,7 +19,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"] # Why does this revert to Google data if Facebook fails?
+      session["devise.facebook_data"] = request.env["omniauth.auth"] # Why does this revert to Google data if Facebook fails?
       redirect_to new_user_registration_url
     end
   end
