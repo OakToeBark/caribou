4b38642d7e72ca616e0041636bb1fd1ec91db436
Brock Luker <brockluker@gmail.com>
2017-01-11 13:49:41 -0800

    Updated ENV settings

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,1 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_m9C8NlSL3GVo7KUVn9P3AS3A',
-  :secret_key => 'sk_test_fK4w86vTf1WmuF7oqL3yp73t'
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
