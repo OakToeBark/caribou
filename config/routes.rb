e644e0490a071e8127d32eeaf1777864703a1c75
Brock Luker <brockluker@gmail.com>
2017-01-19 03:33:09 -0800

    removed subscribers list and changed button size

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => "login", :sign_out => 'logout', :edit => 'profile'},
               :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
 
-  get 'pages/home'
+  get 'pages/home', :to => 'pages#home' # figure out how to call the home page
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
